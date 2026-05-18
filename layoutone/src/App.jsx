90cecf79b03164fd61e335f2bbf0181445cf71b9
pruthvik49 <pruthvirajbhoir070@gmail.com>
2026-05-18 23:07:10 +0530

    tailwind set up is done

---
+++ layoutone/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <main>
+
+        <div className="profile"></div>
+        <div id="bitcoinArea"></div>
+        
+    </main>
+    
+    </>
+  )
+}
+
+export default App
